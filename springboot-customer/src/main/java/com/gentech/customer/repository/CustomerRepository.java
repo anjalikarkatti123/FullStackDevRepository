fc0edfa32943a90f709f90a5c265aed9b41dd3d7
anjalikarkatti123 <144411747+anjalikarkatti123@users.noreply.github.com>
2024-09-20 10:04:14 +0530

    Delete springboot-customer directory

--- springboot-customer/src/main/java/com/gentech/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gentech.customer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gentech.customer.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long>{
-
-}
